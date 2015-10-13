1507dac83d22ec86a738c78edb7b3447b3e532bb
jessonfoo <jessonfoo@gmail.com>
2015-10-13 09:44:44 -0700

    added functions file:w

--- functions.php
+++ functions.php
@@ -79,89 +79,88 @@ function get_img($str){
       foreach($files as $gkey => $gval){
         if (strstr($gval,$fName) == $fName){
           return get_site_url() . strstr($gval,'/wp-content');
+        } else {
+          $file = scandir($d){
+
         }
-      } else {
-        $file = scandir($d){
-
-      }
+        }
       }
     }
-  }
-  if ($size == 1 ){
-    return get_site_url() . '/wp-content/uploads/' . $fName;
-    // return get_site_url() . '/wp-content/uploads/' . date('Y') .'/'.date('m').'/' . $fName; 
+    if ($size == 1 ){
+      return get_site_url() . '/wp-content/uploads/' . $fName;
+      // return get_site_url() . '/wp-content/uploads/' . date('Y') .'/'.date('m').'/' . $fName; 
+    }
   }
 }
 
-function gcbpid ($id){
-  $output= [];
-  $args = array( 'number'=> $number, 'orderby'=> $orderby, 'order'=> $order, 'hide_empty'=> $hide_empty, 'include'=> $ids);
-  $product_categories = get_terms( 'product_cat', $args );
-  foreach($product_categories as $p) {
-    $pid = $p -> parent;
-    if ($pid == $id) {
-      $output[]= $p;
+  function gcbpid ($id){
+    $output= [];
+    $args = array( 'number'=> $number, 'orderby'=> $orderby, 'order'=> $order, 'hide_empty'=> $hide_empty, 'include'=> $ids);
+    $product_categories = get_terms( 'product_cat', $args );
+    foreach($product_categories as $p) {
+      $pid = $p -> parent;
+      if ($pid == $id) {
+        $output[]= $p;
+      }
     }
+    return $output;
   }
-  return $output;
-}
 
-function iBanner($title) {
-  $img='girlpic.jpg,08';
-  echo " <div class='vb-b test full-width f-play color-white' style='background:url("; echo get_img($img); echo ") no-repeat 0% 0%;height:135px;padding-top:10px;'>";
-  echo "<div class='container p-rel mt-50 a-center'>";
-  echo "<h1 class='t-bot f-play color-white'>".$title."</h1></div> </div> ";
-}
-function gznHeader($args=null){
-  $arr = explode(',',$args);
-  $size = sizeof($arr);
-  if ($size > 1){ $str = $arr[2]; }
-  if ($size == 1 ){$img = $args;}
-  if ($str!=''){
-    $string= $str;
-    $sA = explode(' ',$string[0]);
-    $sS = sizeof($sA) - 1;
-    echo '<div class="container mt-30 pb-40 mh-700"> <h1 class="gznH1 f-play" style="font-family:play;">';
-    for($i = 0; $i < $sS; $i++) {
-      echo $sA[$i];
-      echo ' ';
-    }
-    echo '<strong style="display:inline-block;font-family:play;">' . $sA[$sS] . '</strong>   ';
-    if (strlen($img) > 3){
-      echo '<img style="vertical-align: sub;" src="'; echo get_img($img); echo '">';
-    }
-    echo '</h1><div>';
-  } elseif (strlen(get_post_custom_values('content_2')) >= 5 ){ $string=get_post_custom_values('content_2'); }
-  if ($string != ''){
-    $sA = explode(' ',$string[0]);
-    $sS = sizeof($sA) - 1;
-    echo '<div class="container mt-30 pb-40 mh-700"> <h1 class="gznH1 f-play" style="font-family:play;">';
-    for($i = 0; $i < $sS; $i++){
-      echo $sA[$i];
-      echo ' ';
-    }
-    echo '<strong style="display:inline-block;font-family:play;">' . $sA[$sS] . '</strong>   ';
-    if (strlen($img) > 3){
-      echo '<img style="vertical-align: sub;" src="'; echo get_img($img); echo '">';
-    }
-    echo'</h1><div>';
-  } else {
+  function iBanner($title) {
+    $img='girlpic.jpg,08';
+    echo " <div class='vb-b test full-width f-play color-white' style='background:url("; echo get_img($img); echo ") no-repeat 0% 0%;height:135px;padding-top:10px;'>";
+    echo "<div class='container p-rel mt-50 a-center'>";
+    echo "<h1 class='t-bot f-play color-white'>".$title."</h1></div> </div> ";
   }
-}
-function_exists('get_the_title'){
-$string = get_the_title();
-$sA = explode(' ',$string);
-$sS = sizeof($sA) - 1;
-echo '<div class="container mt-30 pb-40 mh-700"> <h1 class="gznH1 f-play" style="font-family:play;">';
-for($i = 0; $i < $sS; $i++) {
-  echo $sA[$i] . ' ';
-}
-echo '<strong style="display:inline-block;font-family:play;">' . $sA[$sS] . '</strong>   ';
-if (strlen($img) > 3){
-  echo '<img style="vertical-align: sub;" src="'; echo get_img($img); echo '">';
-}
-}
-echo'</h1><div></div>';
+  function gznHeader($args=null){
+    $arr = explode(',',$args);
+    $size = sizeof($arr);
+    if ($size > 1){ $str = $arr[2]; }
+    if ($size == 1 ){$img = $args;}
+    if ($str!=''){
+      $string= $str;
+      $sA = explode(' ',$string[0]);
+      $sS = sizeof($sA) - 1;
+      echo '<div class="container mt-30 pb-40 mh-700"> <h1 class="gznH1 f-play" style="font-family:play;">';
+      for($i = 0; $i < $sS; $i++) {
+        echo $sA[$i];
+        echo ' ';
+      }
+      echo '<strong style="display:inline-block;font-family:play;">' . $sA[$sS] . '</strong>   ';
+      if (strlen($img) > 3){
+        echo '<img style="vertical-align: sub;" src="'; echo get_img($img); echo '">';
+      }
+      echo '</h1><div>';
+    } elseif (strlen(get_post_custom_values('content_2')) >= 5 ){ $string=get_post_custom_values('content_2'); }
+    if ($string != ''){
+      $sA = explode(' ',$string[0]);
+      $sS = sizeof($sA) - 1;
+      echo '<div class="container mt-30 pb-40 mh-700"> <h1 class="gznH1 f-play" style="font-family:play;">';
+      for($i = 0; $i < $sS; $i++){
+        echo $sA[$i];
+        echo ' ';
+      }
+      echo '<strong style="display:inline-block;font-family:play;">' . $sA[$sS] . '</strong>   ';
+      if (strlen($img) > 3){
+        echo '<img style="vertical-align: sub;" src="'; echo get_img($img); echo '">';
+      }
+      echo'</h1><div>';
+    } else {
+    }
   }
+  function_exists('get_the_title'){
+  $string = get_the_title();
+  $sA = explode(' ',$string);
+  $sS = sizeof($sA) - 1;
+  echo '<div class="container mt-30 pb-40 mh-700"> <h1 class="gznH1 f-play" style="font-family:play;">';
+  for($i = 0; $i < $sS; $i++) {
+    echo $sA[$i] . ' ';
+  }
+  echo '<strong style="display:inline-block;font-family:play;">' . $sA[$sS] . '</strong>   ';
+  if (strlen($img) > 3){
+    echo '<img style="vertical-align: sub;" src="'; echo get_img($img); echo '">';
+  }
+  }
+  echo'</h1><div></div>';
 }
 ?>
